9af0639ccb477592708d02bd5615007516d47294
Aimee <hiaimeemo@163.com>
2016-12-05 11:41:04 +0800

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set; 
+import java.util.Map.Entry;
+
+public class Test {
+ public static void main(String[] args) {
+  	HashMap<String, String> map = new HashMap<String, String>();
+	map.put("name1", "Andy");
+	map.put("city1","Shanghai");	
+	map.put("name2","Annie");
+	map.put("city2","Beijing");
+
+	Set<Entry<String, String>> sets = map.entrySet();  
+        for(Entry<String, String> entry : sets) {  
+            System.out.print(entry.getKey() + ", ");  
+            System.out.println(entry.getValue());  
+        }  
+}
+}
