c6dd1f0ae7854b740343d33d0bbdfeea53b9eae5
Aimee <hiaimeemo@163.com>
2016-12-07 14:22:38 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
   public static void main(String[] args) {	
 	System.out.println("Hello World!");
 	System.out.println("This is a test!");
+	System.out.println("Test to build 1!");
   }
 }
