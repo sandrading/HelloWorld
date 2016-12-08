411f4f90c5ea150da38b972bf8dd6cdcd24090eb
Aimee <hiaimeemo@163.com>
2016-12-08 08:50:05 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 	System.out.println("Hello World!");
 	System.out.println("This is a test!");
 	System.out.println("Test to build 1-branch1!");
+	  System.out.println("Test trigger1!");	
   }
 }
