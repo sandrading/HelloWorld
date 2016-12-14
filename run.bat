4fe474589e694b7951f3693b390576da664c087d
sandrading <sandra.ding@bleum.com>
2016-12-14 14:44:50 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,20 +1,20 @@
 ﻿@echo off
 echo compile java files
 echo current path:"%cd%"
-rem compile HiTest
-set "PROJECET_NAME=HiTest"
-cd "C:\Program Files (x86)\Jenkins\workspace\%PROJECET_NAME%\"
+rem compile free-style-1
+set "PROJECET_NAME=free-style-1"
+cd "C:\Users\sandra.ding\.jenkins\workspace\%PROJECET_NAME%\"
 javac *.java
 goto SUCCESS
 :SUCCESS
 echo compile done
 echo copy %PROJECET_NAME% class files to Deploy
-cd C:\Users\aimee.mo\Documents\Github\Deploy
+cd C:\Users\sandra.ding\Documents\deploy
 set a="%time:~7%"
 echo %a%
 set path=%PROJECET_NAME%%a%
 mkdir %path%
 echo %path%
-copy "C:\Program Files (x86)\Jenkins\workspace\%PROJECET_NAME%\*.class" C:\Users\aimee.mo\Documents\Github\Deploy\%path%\
+copy "C:\Users\sandra.ding\.jenkins\workspace\%PROJECET_NAME%\*.class" C:\Users\sandra.ding\Documents\deploy\%path%\
 pause
 exit 
