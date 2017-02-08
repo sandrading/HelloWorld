4983bfb898597c904f3c993c30ce94a68d2472d6
sandrading <sandra.ding@bleum.com>
2017-02-08 09:42:52 +0800

    Create run-testNG.bat

---
+++ run-testNG.bat
@@ -0,0 +1,19 @@
+echo compile java files
+echo current path:"%cd%"
+rem compile free-style-1
+set "PROJECET_NAME=free-style-1"
+cd "C:\Users\sandra.ding\.jenkins\workspace\%PROJECET_NAME%\"
+javac *.java
+goto SUCCESS
+:SUCCESS
+echo compile done
+echo copy %PROJECET_NAME% class files to Deploy
+cd C:\Users\sandra.ding\Documents\deploy
+set a="%time:~7%"
+echo %a%
+set path=%PROJECET_NAME%%a%
+mkdir %path%
+echo %path%
+copy "C:\Users\sandra.ding\.jenkins\workspace\%PROJECET_NAME%\*.class" C:\Users\sandra.ding\Documents\deploy\%path%\
+pause
+exit 
